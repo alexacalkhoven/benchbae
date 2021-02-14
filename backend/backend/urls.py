3ffb3b468338f177b26f166ca68e2ebcd463a866
Alexa Calkhoven <alexa.calkhoven1@ucalgary.ca>
2021-02-13 18:25:35 -0700

    End point to get closest bench added.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from hackathon.views import PopulateDatabase, BenchView
+from hackathon.views import PopulateDatabase, BenchView, ClosestBench
 from rest_framework import routers
 
 
@@ -25,6 +25,6 @@ router.register(r'bench', BenchView)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('populate-db', PopulateDatabase.as_view()),
-    path('api/', include(router.urls)),
-    
+    path('closest-bench', ClosestBench.as_view()),
+    path('api/', include(router.urls)),   
 ]
--- backend/hackathon/views.py
+++ backend/hackathon/views.py
@@ -5,10 +5,29 @@ from rest_framework.views import APIView
 from rest_framework import viewsets
 from rest_framework.response import Response
 from django.core import serializers
+from .computation import CoordinateHandler
 import csv
 import json
 
 # Create your views here.
+
+class ClosestBench(APIView):
+    def get(self, request, format=None):
+        comp = CoordinateHandler()
+        user_long = float(request.query_params["long"])
+        user_lat = float(request.query_params["lat"])
+        bench_list = Bench.objects.all().values()
+        # default values
+        min_dist = -1
+        closest_bench = None
+        # traverse benches in database, calc distance for each one
+        for bench in bench_list:
+            dist = comp.distance(float(bench['latitude']), float(bench['longitude']), user_lat, user_long)
+            if(min_dist == -1 or min_dist > dist):
+                min_dist = dist
+                closest_bench = bench
+        return Response(closest_bench)
+
 
 class BenchView(viewsets.ReadOnlyModelViewSet):
     serializer_class = BenchSerializer
@@ -24,16 +43,16 @@ class PopulateDatabase(APIView):
         with open('data/benches.csv') as file:
             reader = csv.DictReader(file)
             for row in reader:
-                bench = Bench(
-                    pk=index,
-                    latitude=row['latitude'],
-                    longitude=row['longitude'],
-                    location_detail=row['LOCATION_DETAIL'],
-                    orientation=row['ORIENTATION'],
-                    life_cycle_status=row['LIFE_CYCLE_STATUS']
-                )
-
-                benches.append(bench)
+                if(row["ORIENTATION"] == "NW" or row["ORIENTATION"] == "W" or row["ORIENTATION"] == "SW"):
+                    bench = Bench(
+                        pk=index,
+                        latitude=row['latitude'],
+                        longitude=row['longitude'],
+                        location_detail=row['LOCATION_DETAIL'],
+                        orientation=row['ORIENTATION'],
+                        life_cycle_status=row['LIFE_CYCLE_STATUS']
+                    )
+                    benches.append(bench)
                 index += 1
         
         print('Created all benches from csv file')
