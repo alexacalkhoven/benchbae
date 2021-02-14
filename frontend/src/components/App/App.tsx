184c16fea34362624395a6ef7c5bd90674afb4a6
Tyler Yip <tyler.yip1@ucalgary.ca>
2021-02-13 17:17:08 -0700

    Feat: Added the landing page

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import LandingPage from './landing_page/LandingPage';
 
 const App = () => {
-  return <div className="app-container">
-    <Router>
-      <Switch>
-        <Route path="/" />
-      </Switch>
-    </Router>
-  </div>;
+  return (
+    <div className="app-container">
+      <Router>
+        <Switch>
+          <Route path="/" component={LandingPage} />
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
