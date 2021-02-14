829ce64b0e417ad46e2ed21d87e10134ad5b4ffe
Radu Schirliu <radu.schirliu@gmail.com>
2021-02-13 17:35:09 -0700

    fix: Pandas error

--- backend/hackathon/views.py
+++ backend/hackathon/views.py
@@ -5,7 +5,6 @@ from rest_framework.views import APIView
 from rest_framework import viewsets
 from rest_framework.response import Response
 from django.core import serializers
-import pandas as pd
 import csv
 import json
 
