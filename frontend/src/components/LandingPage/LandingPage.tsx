329905ed7f0d7a5b510a1158d25ac534557e3e87
Tyler Yip <tyler.yip1@ucalgary.ca>
2021-02-13 18:10:52 -0700

    Feat: Good styles for landing page

--- frontend/src/components/LandingPage/LandingPage.tsx
+++ frontend/src/components/LandingPage/LandingPage.tsx
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 import './LandingPage.css';
 import Logo from '../../assets/benchbae8.png';
 import TwoPeople from '../../assets/two-people-standing.svg';
+import BlueWomanSitting from '../../assets/blue-woman-sitting.svg';
 
 function LandingPage() {
   return (
     <div className="landingpage-main">
       <img src={Logo} alt="Logo" className="logo"></img>
-      <h1>BenchBae</h1>
+      <h1 className="bench-bae">BenchBae</h1>
       <img src={TwoPeople} alt="Logo" className="two-people"></img>
       <button className="bench-button">Bench Me Bitch</button>
+      <img
+        src={BlueWomanSitting}
+        alt="Blue Woman Sitting"
+        className="blue-woman-sitting"
+      ></img>
+      <p className="motivation">
+        This is a paragraph about why we made this thing.
+      </p>
     </div>
   );
 }
