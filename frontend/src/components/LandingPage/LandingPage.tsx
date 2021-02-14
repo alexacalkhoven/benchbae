8783ade2be17446449e6a57425f34e9d8292f7b5
Radu Schirliu <radu.schirliu@gmail.com>
2021-02-14 09:16:52 -0700

    feat: Landing page refresh

--- frontend/src/components/LandingPage/LandingPage.tsx
+++ frontend/src/components/LandingPage/LandingPage.tsx
@@ -1,9 +1,9 @@
 import Nav from '../Nav/Nav';
 import './LandingPage.css';
-import TwoPeople from '../../assets/two-people-standing.svg';
-import ManPurpleStanding from '../../assets/man-standing-purple.svg';
-import WomanPinkSitting from '../../assets/woman-sitting-plants-pink.svg';
-import PlantsPlain from '../../assets/plants-plain.svg';
+import HealthIcon from '../../assets/health-icon.png';
+import CovidIcon from '../../assets/covid-icon.png';
+import DateIcon from '../../assets/date-icon.png';
+import BenchIcon from '../../assets/bench_icon.png';
 import LandingImage from '../../assets/landing-image-4.png';
 import BenchBaeContext from '../../contexts/BenchBaeContext';
 import { useContext, useRef } from 'react';
@@ -38,12 +38,8 @@ const LandingPage = () => {
         <div className="landingpage-front">
           <Nav fade={true} />
 
-          {/* <img src={TwoPeople} alt="Logo" className="landingpage-image"/> */}
-          {/* <img src={PlantsPlain} alt="Plants" className="landingpage-image" /> */}
-
-          <div className="landingpage-image">
-            {/* <img className="landingpage-image1" src={WomanPinkSitting} alt="" />
-            <img className="landingpage-image2" src={ManPurpleStanding} alt="" /> */}
+          <div className="landingpage-image-container">
+            <p className="landingpage-image-pretext">Click below to find your BenchBae date combination!</p>
             <img className="landingpage-image" src={LandingImage} alt="" />
           </div>
 
@@ -55,17 +51,84 @@ const LandingPage = () => {
             </div>
           ) : (
             <button className="landingpage-button" onClick={() => findBench()}>
-              Bench Me
+              Find your date
             </button>
           )}
 
           <div className="landingpage-expand-icon arrow-bounce">
-            <span className="material-icons" onClick={() => scroll()}>expand_more</span>
+            <span className="material-icons" onClick={() => scroll()}>
+              expand_more
+            </span>
           </div>
         </div>
 
         <div className="motivation-container" ref={motivationRef}>
-          <p className="motivation">
+          <p className="motivation-title">About Us</p>
+          <p className="motivation-pretext">
+            BenchBae was inspired by a dataset detailing the city’s memorial and
+            park bench data from Calgary’s municipal open-source data. It’s no
+          </p>
+          <p className="motivation-content">
+            <div className="motivation-section">
+              <div className="motivation-icon">
+                <img src={CovidIcon} alt="Covid" />
+              </div>
+              <p>
+                BenchBae was inspired by a dataset detailing the city’s memorial
+                and park bench data from Calgary’s municipal open-source data.
+                It’s no secret that the covid-19 pandemic has had devastating
+                effects on our local economy. From temporary closures to reduced
+                capacity, increased sanitization costs and significantly lower
+                revenue, Calgarian restaurants have been slowly suffocating
+                under the weight of the economic downturn. A survey conducted by
+                Statistics Canada in October 2020 found that 57 percent of
+                businesses in the hospitality and food services industry would
+                be unable to take on more debt. Nearly 30 percent of businesses
+                in the sector reported that they would be operationally viable
+                for only 6 more months until facing drastic cost-cutting
+                measures.
+              </p>
+            </div>
+            <div className="motivation-section">
+              <div className="motivation-icon">
+                <img src={HealthIcon} alt="Mental health" />
+              </div>
+              <p>
+                Another issue to raise could be the state of mental health in
+                Canada. Due to covid-19, the mental health of many Calgarians
+                has deteriorated. Because of this pandemic, 24% of participants
+                in a study reported fair or poor mental health. This is a
+                massive jump from the previous 2018 survey where 8% of Canadians
+                reported fair or poor mental health. Most participants
+                experienced at least one symptom of anxiety in the two weeks
+                prior to completing the survey.
+              </p>
+            </div>
+            <div className="motivation-section">
+              <div className="motivation-icon">
+                <img src={DateIcon} alt="Date night" />
+              </div>
+              <p>
+                BenchBae puts an innovative twist on the standard date night
+                process while keeping mental health in mind. Our application
+                lets our users find the closest bench west-facing to watch the
+                sunset with their partner. This spontaneous and intimate
+                experience is elevated with a hearty meal from a nice nearby
+                restaurant as recommended by our app. The result is an
+                experience that not only stimulates the local economy by
+                supporting Calgary restaurants, but makes it easy to enjoy the
+                company of your loved ones under the picturesque backdrop of the
+                sun setting over the Rocky Mountains. For those early birds who
+                love grabbing a bite at one of Calgary’s numerous breakfast
+                joints, the application can also identify east-facing benches to
+                add a much needed dose of sunshine into your morning routine.
+                You can sit back, take a nice sip of your morning coffee, and
+                watch the sun peak over the horizon.
+              </p>
+            </div>
+          </p>
+
+          {/* <p className="motivation">
             BenchBae was inspired by a dataset detailing the city’s memorial and
             park bench data from Calgary’s municipal open-source data.
           </p>
@@ -112,7 +175,7 @@ const LandingPage = () => {
             connection. Our application not only promotes spending within our
             local economy, it also encourages safe social connection, and
             inspires users to enjoy the little things in life.
-          </p>
+          </p> */}
         </div>
       </div>
     </div>
