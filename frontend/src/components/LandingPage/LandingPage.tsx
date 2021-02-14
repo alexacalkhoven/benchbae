988b193d6c57cee621f555e83de78e063fa2d918
Radu Schirliu <radu.schirliu@gmail.com>
2021-02-14 09:37:51 -0700

    feat: landing copy & more firebase

--- frontend/src/components/LandingPage/LandingPage.tsx
+++ frontend/src/components/LandingPage/LandingPage.tsx
@@ -39,7 +39,9 @@ const LandingPage = () => {
           <Nav fade={true} />
 
           <div className="landingpage-image-container">
-            <p className="landingpage-image-pretext">Click below to find your BenchBae date combination!</p>
+            <p className="landingpage-image-pretext">
+              Click below to find your BenchBae date combination!
+            </p>
             <img className="landingpage-image" src={LandingImage} alt="" />
           </div>
 
@@ -65,8 +67,9 @@ const LandingPage = () => {
         <div className="motivation-container" ref={motivationRef}>
           <p className="motivation-title">About Us</p>
           <p className="motivation-pretext">
-            BenchBae was inspired by a dataset detailing the city’s memorial and
-            park bench data from Calgary’s municipal open-source data. It’s no
+            BenchBae was inspired by a dataset detailing Calgary’s park benches
+            from Calgary’s municipal open-source data. Read more about our story
+            below!
           </p>
           <p className="motivation-content">
             <div className="motivation-section">
@@ -74,19 +77,16 @@ const LandingPage = () => {
                 <img src={CovidIcon} alt="Covid" />
               </div>
               <p>
-                BenchBae was inspired by a dataset detailing the city’s memorial
-                and park bench data from Calgary’s municipal open-source data.
-                It’s no secret that the covid-19 pandemic has had devastating
+                It’s no secret that the COVID-19 pandemic has had devastating
                 effects on our local economy. From temporary closures to reduced
                 capacity, increased sanitization costs and significantly lower
                 revenue, Calgarian restaurants have been slowly suffocating
                 under the weight of the economic downturn. A survey conducted by
-                Statistics Canada in October 2020 found that 57 percent of
-                businesses in the hospitality and food services industry would
-                be unable to take on more debt. Nearly 30 percent of businesses
-                in the sector reported that they would be operationally viable
-                for only 6 more months until facing drastic cost-cutting
-                measures.
+                Statistics Canada showed that nearly 30 percent of businesses in
+                the Food and Drink sector reported that they would be
+                operationally viable for only six more months until facing
+                drastic measures. We aim to help this problem by giving more
+                exposure to Calgary’s restaurants, stimulating our economy.
               </p>
             </div>
             <div className="motivation-section">
@@ -94,14 +94,16 @@ const LandingPage = () => {
                 <img src={HealthIcon} alt="Mental health" />
               </div>
               <p>
-                Another issue to raise could be the state of mental health in
-                Canada. Due to covid-19, the mental health of many Calgarians
+                Mental health concerns have become more and more pressing over
+                the years. Due to COVID-19, the mental health of many Calgarians
                 has deteriorated. Because of this pandemic, 24% of participants
                 in a study reported fair or poor mental health. This is a
                 massive jump from the previous 2018 survey where 8% of Canadians
                 reported fair or poor mental health. Most participants
                 experienced at least one symptom of anxiety in the two weeks
-                prior to completing the survey.
+                prior to completing the survey. We strive to contribute a safe,
+                new way for friends, family and couples around Calgary to
+                relieve some stress and get out of the house.
               </p>
             </div>
             <div className="motivation-section">
@@ -110,20 +112,17 @@ const LandingPage = () => {
               </div>
               <p>
                 BenchBae puts an innovative twist on the standard date night
-                process while keeping mental health in mind. Our application
-                lets our users find the closest bench west-facing to watch the
-                sunset with their partner. This spontaneous and intimate
-                experience is elevated with a hearty meal from a nice nearby
-                restaurant as recommended by our app. The result is an
-                experience that not only stimulates the local economy by
-                supporting Calgary restaurants, but makes it easy to enjoy the
-                company of your loved ones under the picturesque backdrop of the
-                sun setting over the Rocky Mountains. For those early birds who
-                love grabbing a bite at one of Calgary’s numerous breakfast
-                joints, the application can also identify east-facing benches to
-                add a much needed dose of sunshine into your morning routine.
-                You can sit back, take a nice sip of your morning coffee, and
-                watch the sun peak over the horizon.
+                process. Our application lets users find the closest west-facing
+                bench to watch the sunset with their partner. This spontaneous
+                and intimate experience is elevated with a hearty meal from a
+                nearby restaurant as recommended by our app. The result is an
+                experience that not only stimulates the local economy but allows
+                users to safely enjoy the company of their loved ones with a
+                beautiful Calgary sunset backdrop. In the future, we hope to
+                continually improve BenchBae and magnify our impact on Calgary’s
+                future. We would love to focus our restaurant recommendations on
+                local businesses, further helping the business owners who need
+                it most.
               </p>
             </div>
           </p>
